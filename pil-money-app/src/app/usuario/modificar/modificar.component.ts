79863698745b95a3256a2d0c13d54e3924a9e0d3
Santiago Biolatto <santiagobiolatto96@gmail.com>
2021-07-16 21:34:53 -0300

    Testeando version de booststrap

--- pil-money-app/src/app/usuario/modificar/modificar.component.ts
+++ pil-money-app/src/app/usuario/modificar/modificar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modificar',
   templateUrl: './modificar.component.html',
-  styleUrls: ['./modificar.component.css']
+  styleUrls: ['./modificar.component.css'],
 })
 export class ModificarComponent implements OnInit {
 
-  constructor() { }
+  modificarForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+  
   ngOnInit(): void {
+    this.modificarForm = this.formBuilder.group({
+      nombreCompleto: ['', Validators.compose([Validators.minLength(3), Validators.required])],
+      email: ['', Validators.compose([Validators.email, Validators.required])],
+      dni: ['', Validators.compose([Validators.minLength(7), Validators.maxLength(8), Validators.required])],
+      sexo: ['', Validators.compose([Validators.required])]
+    });
   }
-
 }
