edcb8b2afbf54af16d4a8cce60b56dd5697dbc5d
Tomás Malvissi <tomasmalvissi@hotmail.com>
2021-07-20 12:55:26 -0300

    login y registrar terminados

--- pil-money-app/src/app/pages/login/login.component.ts
+++ pil-money-app/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.email, Validators.required])],
+      contraseña:[Validators.minLength(8), Validators.maxLength(12), Validators.required],
+    });
   }
-
 }
--- pil-money-app/src/app/usuario/crear/crear.component.ts
+++ pil-money-app/src/app/usuario/crear/crear.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-crear',
@@ -19,8 +20,44 @@ export class CrearComponent implements OnInit {
       dni: ['', Validators.compose([Validators.minLength(7), Validators.maxLength(8), Validators.required])],
       sexo: ['', Validators.compose([Validators.required])],
       fechaDeNacimiento: ['', Validators.compose([Validators.required])],
-      nuevaPassword:[Validators.compose([Validators.minLength(8), Validators.maxLength(12), Validators.required])],
+      contraseña:[Validators.minLength(8), Validators.maxLength(12), Validators.required],
+      confirmarContraseña:[Validators.minLength(8), Validators.maxLength(12), Validators.required],
     });
     this.crearForm.valueChanges.subscribe(console.log)
+  }
+  
+  enviarForm() {
+    if (this.crearForm.valid) {
+      Swal.fire({
+        icon: 'success',
+        title: 'Registrar',
+        text: 'Usuario registrado con éxito',
+        confirmButtonText: 'Aceptar',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            icon: 'info',
+            title: 'Datos guardados',
+            html: `
+            <ul style="list-style: none;">
+            <li><strong>Nombre completo:</strong> ${this.crearForm.value.nombreCompleto}</li>
+            <li><strong>Email:</strong> ${this.crearForm.value.email}</li>
+            <li><strong>Fecha de nacimiento:</strong> ${this.crearForm.value.fechaDeNacimiento}</li>
+            <li><strong>DNI:</strong> ${this.crearForm.value.dni}</li>
+            <li><strong>Sexo:</strong> ${this.crearForm.value.sexo}</li>
+            </ul>`,
+            confirmButtonText: 'Aceptar',
+          });
+        }
+      });
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Registrar',
+        text: 'Algunos campos son inválidos',
+        confirmButtonText: 'Aceptar',
+      });
+      this.crearForm.markAllAsTouched();
+    }
   }
 }
