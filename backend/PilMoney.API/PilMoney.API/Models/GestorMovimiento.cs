711cf460209907691ecd44b47810ee84a4461e29
Santiago Biolatto <santiagobiolatto96@gmail.com>
2021-09-20 05:24:52 -0300

    Test sin entity

---
+++ backend/PilMoney.API/PilMoney.API/Models/GestorMovimiento.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace PilMoney.API.Models
+{
+    public class GestorMovimiento
+    {
+
+        public void AgregarMovimiento(Movimiento movimiento)
+        {
+            string connection = ConfigurationManager.ConnectionStrings["database"].ToString();
+
+            using (SqlConnection sqlConnection = new SqlConnection(connection))
+            {
+                sqlConnection.Open();
+
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.CommandText = "insertar_movimiento";
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.Add(new SqlParameter("@Id_Cuenta", movimiento.Id_Cuenta1));
+                sqlCommand.Parameters.Add(new SqlParameter("@Tipo_Movimiento", movimiento.Tipo_Movimiento1));
+                sqlCommand.Parameters.Add(new SqlParameter("@Fecha_Hora", movimiento.Fecha_Hora1));
+                sqlCommand.Parameters.Add(new SqlParameter("@Monto", movimiento.Monto1));
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+
+    }
+}
---
+++ backend/PilMoney.API/PilMoney.API/Models/Movimiento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PilMoney.API.Models
+{
+    public class Movimiento
+    {
+        private int Id;
+        private DateTime Fecha_Hora;
+        private float Monto;
+        private string Tipo_Movimiento;
+        private int Id_Cuenta;
+
+        public Movimiento()
+        {
+        }
+
+        public Movimiento(int id, DateTime fecha_Hora, float monto, string tipo_Movimiento, int id_Cuenta)
+        {
+            Id1 = id;
+            Fecha_Hora1 = fecha_Hora;
+            Monto1 = monto;
+            Tipo_Movimiento1 = tipo_Movimiento;
+            Id_Cuenta1 = id_Cuenta;
+        }
+
+        public int Id1 { get => Id; set => Id = value; }
+        public DateTime Fecha_Hora1 { get => Fecha_Hora; set => Fecha_Hora = value; }
+        public float Monto1 { get => Monto; set => Monto = value; }
+        public string Tipo_Movimiento1 { get => Tipo_Movimiento; set => Tipo_Movimiento = value; }
+        public int Id_Cuenta1 { get => Id_Cuenta; set => Id_Cuenta = value; }
+    }
+}
