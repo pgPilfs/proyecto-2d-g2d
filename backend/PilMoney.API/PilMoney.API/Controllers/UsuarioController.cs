0952c10ea7930027911d374740005a3092e6db1c
vero-m5 <vero.m1897@gmail.com>
2021-09-20 14:42:41 -0300

    usuario controller prueba

---
+++ backend/PilMoney.API/PilMoney.API/Controllers/UsuarioController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using PilMoney.API.Models.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using PilMoney.API.Models.Entities;
+
+namespace PilMoney.API.Controllers
+{
+	public class UsuarioController : Controllers
+	{
+		private UsuarioService _usuarioService;
+		public UsuarioController()
+		{
+			_usuarioService = new UsuarioService();
+		}
+		// POST: api/Usuario
+		public Usuario Post(Usuario usuario)
+		{
+			_usuarioService.Guardar(usuario);
+			return usuario;
+		}
+		// PUT: api/Cuenta/id
+		public Usuario Put(int id, [FromBody] Usuario usuario)
+		{
+			_usuarioService.Modificar(id, usuario);
+			return usuario;
+		}
+		
+	}
+}
+
+
---
+++ backend/PilMoney.API/PilMoney.API/Models/Services/UsuarioService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PilMoney.API.Models.Services
+{
+	public class UsuarioService
+	{
+		private readonly ApplicationDbContext _context;
+		public UsuarioService(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public Usuario Guardar(Usuario usuario)
+		{
+
+			_context.Usuarios.Add(usuario);
+			_context.SaveChanges();
+
+			return usuario;
+		}
+		public Usuario Modificar(int Id, Usuario usuario)
+		{
+			var usuarioEncontrado = _context.Usuarios.Where(x => x.Id == Id).FirstOrDefault();
+			if (usuarioEncontrado != null)
+			{
+				usuarioEncontrado.NombreCompleto = usuario.NombreCompleto;
+				usuarioEncontrado.DNI = usuario.DNI;
+				usuarioEncontrado.Sexo = usuario.Sexo;
+				usuarioEncontrado.Fecha_Nac = usuario.Fecha_Nac;
+				usuarioEncontrado.Email = usuario.Email;
+				usuarioEncontrado.Contraseña = usuario.Contraseña;
+				usuarioEncontrado.Id_Cuenta = usuario.Id_Cuenta;
+
+				_context.Usuarios.Add(usuarioEncontrado);
+				_context.SaveChanges();
+			}
+			return usuario;
+		}
+	}
+}
+
+
