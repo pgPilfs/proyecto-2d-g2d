264657da6578e595082e701c6d4170b17dc3a231
vero-m5 <vero.m1897@gmail.com>
2021-09-25 22:11:16 -0300

    Actualización usuariosController CORS

--- backend/PilMoney.API/PilMoney.API/Controllers/UsuariosController.cs
+++ backend/PilMoney.API/PilMoney.API/Controllers/UsuariosController.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.Description;
 using PilMoney.API.Models;
 
 namespace PilMoney.API.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class usuariosController : ApiController
     {
         private ModelsConfig db = new ModelsConfig();
