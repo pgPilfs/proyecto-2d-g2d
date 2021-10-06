288f3562bc6c83d4ed2263e220e947a776273a94
Santiago Biolatto <santiagobiolatto96@gmail.com>
2021-10-06 16:37:19 -0300

    Cerrar sesion listo

--- frontend/src/app/layout/header/header.component.ts
+++ frontend/src/app/layout/header/header.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  cerrarSesion() {
+    if (localStorage.getItem('auth')) {
+      localStorage.removeItem('auth');
+    }
+    this.router.navigate(['/login']);
   }
-
 }
